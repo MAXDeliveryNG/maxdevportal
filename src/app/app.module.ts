fc225c23db045e459b255e8cb732dc534e46fae2
Adesina Oyindamola <dsnoyin27@gmail.com>
2017-10-30 14:54:40 +0100

    All default tests passed. Imported all declarations, providers and modules to the spec.ts file. Also rendered the title in h1 tag and changed 'Welcome to app!!' to 'app'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [
     Title,
