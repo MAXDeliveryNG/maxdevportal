cd1169ef73e672877e9f048c1188fa701b06cb2c
Patrick <patrickfoh@gmail.com>
2017-10-30 15:30:54 +0100

    Update routing

--- src/app/site/site-routing.module.ts
+++ src/app/site/site-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
         path: 'login',
         component: LoginComponent,
         data: {
-            title: 'Login'
+            title: 'MAX API | Login'
         }
     }
 ]
