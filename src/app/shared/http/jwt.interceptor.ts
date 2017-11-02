c53cb34892b9b5da68d9250206b215acc7985f15
Patrick <patrickfoh@gmail.com>
2017-11-02 13:47:33 +0100

    Update interceptor

--- src/app/shared/http/jwt.interceptor.ts
+++ src/app/shared/http/jwt.interceptor.ts
@@ -2,21 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { JwtService } from '../../services';
+import { SessionService } from '../../services';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
-    constructor(private jwtService: JwtService) { }
+    constructor(private session: SessionService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if(this.jwtService.readToken()) {
+        if (this.session.getToken()) {
             const request = req.clone({
                 setHeaders: {
-                    Authorization: `Bearer ${this.jwtService.readToken()}`
+                    Authorization: `Bearer ${this.session.getToken()}`
                 }
             });
-
             return next.handle(request);
         } else {
             return next.handle(req);
