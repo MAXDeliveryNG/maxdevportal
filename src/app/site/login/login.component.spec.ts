c43f7a13399a63fe663c0e07e1183d96f2121ba7
Patrick <patrickfoh@gmail.com>
2017-11-02 14:57:20 +0100

    Update test spec

--- src/app/site/login/login.component.spec.ts
+++ src/app/site/login/login.component.spec.ts
@@ -1,11 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { PersistenceModule } from 'angular-persistence';
 import { FormsModule} from '@angular/forms';
 import { ClarityModule } from 'clarity-angular';
+import { JwtHelper } from 'angular2-jwt';
 
 import { LoginComponent } from './login.component';
-import { AuthService } from '../../services'
+import { SessionService, AuthService } from '../../services'
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -14,8 +16,14 @@ describe('LoginComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports: [FormsModule, HttpClientTestingModule, RouterTestingModule, ClarityModule],
-      providers: [AuthService]
+      imports: [
+        FormsModule, 
+        HttpClientTestingModule, 
+        RouterTestingModule, 
+        PersistenceModule, 
+        ClarityModule
+      ],
+      providers: [JwtHelper, SessionService, AuthService]
     })
     .compileComponents();
   }));
