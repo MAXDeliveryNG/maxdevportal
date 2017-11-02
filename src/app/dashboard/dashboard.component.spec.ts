c4af3bb77e9f7cb76820ddf8fc069a2e13db56ef
Patrick <patrickfoh@gmail.com>
2017-11-02 13:56:05 +0100

    Update test spec

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,8 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { PersistenceModule } from 'angular-persistence';
+import { JwtHelper } from 'angular2-jwt';
 import { ClarityModule } from 'clarity-angular';
 import { DashboardComponent } from './dashboard.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { SessionService } from '../services'
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -10,8 +13,9 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ RouterTestingModule, ClarityModule.forRoot() ],
-      declarations: [ DashboardComponent, HeaderComponent ]
+      imports: [ RouterTestingModule, PersistenceModule ,ClarityModule.forRoot() ],
+      declarations: [ DashboardComponent, HeaderComponent ],
+      providers: [JwtHelper, SessionService]
     })
     .compileComponents();
   }));
