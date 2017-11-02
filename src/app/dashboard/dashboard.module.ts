bd6cbf0f43c9d18223ac43b802cba107a77c2265
Patrick <patrickfoh@gmail.com>
2017-11-02 13:45:47 +0100

    Provide SessionService

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { ClarityModule } from 'clarity-angular';
+
+import { SessionService } from '../services'
+
 import { DashboardComponent } from './dashboard.component';
 import { HeaderComponent } from './layout/header/header.component'
 
@@ -13,6 +16,7 @@ import { HeaderComponent } from './layout/header/header.component'
     ClarityModule.forRoot(),
     DashboardRoutingModule
   ],
-  declarations: [DashboardComponent, HeaderComponent]
+  declarations: [DashboardComponent, HeaderComponent],
+  providers: [SessionService]
 })
 export class DashboardModule { }
