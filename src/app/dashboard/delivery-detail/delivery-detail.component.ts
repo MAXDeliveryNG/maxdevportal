3ce92ecb210c3202e7bbe766c4af307b51aa3bfb
Patrick Foh <patrickfoh@gmail.com>
2017-12-13 12:57:46 +0100

    refactor component to manage observable manually

--- src/app/dashboard/delivery-detail/delivery-detail.component.ts
+++ src/app/dashboard/delivery-detail/delivery-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { OrderService } from 'app/services';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/operator/catch';
 import { Subject } from 'rxjs';
 import { GeoUtils } from 'app/shared/utils/map';
 
@@ -13,9 +14,13 @@ import { GeoUtils } from 'app/shared/utils/map';
 })
 export class DeliveryDetailComponent implements OnInit {
 
-  order$: Observable<any>;
   route$: Subscription;
   cancel$: Subject<any> = new Subject();
+
+  loading: boolean;
+  loadingError: boolean = false;
+  orderID: string;
+  order: any;
 
   mapDefaultCenter: object = {
     lat: 6.4811612,
@@ -29,28 +34,49 @@ export class DeliveryDetailComponent implements OnInit {
     private orders: OrderService
   ) { }
 
+  private transformAddresses(response) {
+    response.data.delivery_address.coordinates = GeoUtils.toLatLngObject(response.data.delivery_address.coordinates);
+    response.data.pickup_address.coordinates = GeoUtils.toLatLngObject(response.data.pickup_address.coordinates);
+
+    return response;
+  } 
+
+  private transformDates(response) {
+    response.data.order_status.map(status => {
+      status.created_at = status.created_at * 1000;
+      status.status = status.status.toUpperCase();
+      return status;
+    })
+
+    response.data.pickup_window.date = response.data.pickup_window.date * 1000;
+    response.data.created_at = response.data.created_at * 1000;
+
+    return response
+  }
+
+  fetchOrder(id: string) {
+    this.loading = true;
+    this.loadingError = false;    
+
+    this.orders.get(id)
+      .map(this.transformAddresses)
+      .map(this.transformDates)
+      .takeUntil(this.cancel$)
+      .subscribe(response => {
+        this.order = response.data;
+      }, err => {
+        this.loading = false;
+        this.loadingError = true;
+      }, () => {
+        this.loading = false;
+      });
+  }
+
   ngOnInit() {
     this.route$ = this.route.params
       .subscribe(params => {
-        this.order$ = this.orders.get(params.id)
-          .map((response: any) => {
-            response.data.delivery_address.coordinates = GeoUtils.toLatLngObject(response.data.delivery_address.coordinates);
-            response.data.pickup_address.coordinates = GeoUtils.toLatLngObject(response.data.pickup_address.coordinates);
-
-            response.data.pickup_window.date = response.data.pickup_window.date * 1000;
-            response.data.created_at = response.data.created_at * 1000;
-
-            return response;
-          })
-          .map((response: any) => {
-            response.data.order_status.map(status => {
-              status.created_at = status.created_at * 1000;
-              status.status = status.status.toUpperCase();
-              return status;
-            })
-            return response;
-          })
-          .takeUntil(this.cancel$);
+        this.orderID = params.id;
+        this.fetchOrder(params.id)
       });
   }
 
