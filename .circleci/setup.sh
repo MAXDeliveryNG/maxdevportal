3e810bf4f3163a148be108bf9c533074e1221ece
Patrick Foh <patrickfoh@gmail.com>
2017-12-14 14:16:15 +0100

    Update setup script

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#git remote add production git@devs-staging.max.ng:~/analytics
+git remote add production git@developers.max.ng:~/
 git remote add staging git@devs-staging.max.ng:~/
 
 # Add analytics.max.ng to the list of known hosts
-#ssh-keyscan -H analytics.max.ng >> ~/.ssh/known_hosts
+ssh-keyscan -H developers.max.ng >> ~/.ssh/known_hosts
 ssh-keyscan -H devs-staging.max.ng >> ~/.ssh/known_hosts
