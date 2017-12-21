b0e90fb8091145052e1b75472dd14f6d873d299e
Patrick Foh <patrickfoh@gmail.com>
2017-12-21 09:33:47 +0100

    Add App interface

---
+++ src/app/models/data/app.ts
@@ -0,0 +1,12 @@
+import { AppKey } from "app/models/data/app-key";
+
+export interface App {
+    id?: string;
+    user_id?: string;
+    name: string;
+    description?: string;
+    webhook_url?: string;
+    keys?: AppKey[];
+    updated_at: Number;
+    created_at: Number
+}
