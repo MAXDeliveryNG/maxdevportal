413013cede1a3c111a6907316223348cccc44b32
Patrick Foh <patrickfoh@gmail.com>
2017-12-21 08:09:38 +0100

    Add AppDetailComponent class

--- src/app/dashboard/app-detail/app-detail.component.ts
+++ src/app/dashboard/app-detail/app-detail.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AppService } from 'app/services';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-app-detail',
   templateUrl: './app-detail.component.html',
   styleUrls: ['./app-detail.component.scss']
 })
+
 export class AppDetailComponent implements OnInit {
 
-  constructor() { }
+  route$: Subscription;
+  loading: boolean = true;
+  loadingError: boolean = false;
+  data: any;
+  id: string;
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute, 
+    private appService: AppService) { }
+
+  private transform(response) {
+    response.data.keys.map(key => {
+      key.visible = false;
+      key.created_at = key.created_at * 1000;
+      key.updated_at = key.updated_at * 1000;
+      return key;
+    })
+    return response;
+  }
+  
+  fetchData(id: string) {
+    this.loadingError = false;
+
+    this.appService.read(id)
+      .map(this.transform)
+      .subscribe(response => {
+        this.data = response.data;
+        this.loadingError = false;
+      }, err => {
+        this.loading = false;
+        this.loadingError = true;
+      }, () => {
+        this.loading = false
+      })
   }
 
+  ngOnInit() {
+    this.route$ = this.route.params.subscribe(params => {
+      this.id = params.id;
+      this.fetchData(params.id);
+    })
+  }
 }
