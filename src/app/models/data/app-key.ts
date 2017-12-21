f1248c1838158f665f2eb5351d14cbee6027d0db
Patrick Foh <patrickfoh@gmail.com>
2017-12-21 16:06:04 +0100

    Add AppKey interface

--- src/app/models/data/app-key.ts
+++ src/app/models/data/app-key.ts
@@ -2,10 +2,11 @@ export interface AppKey {
     id?: string;
     app_id?: string;
     user_id?: string;
-    is_revoked?: boolean
+    is_revoked?: boolean;
+    visible?: boolean;
     name: string;
-    private_key: string;
-    public_key: string;
+    private_key?: string;
+    public_key?: string;
     updated_at?: Number;
     created_at?: Number
 }
