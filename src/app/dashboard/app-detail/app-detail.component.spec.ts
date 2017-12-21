8326081c0d755a0c7baaa5e9019e304629d14a21
Patrick Foh <patrickfoh@gmail.com>
2017-12-21 08:28:18 +0100

    Update component spec

--- src/app/dashboard/app-detail/app-detail.component.spec.ts
+++ src/app/dashboard/app-detail/app-detail.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppDetailComponent } from './app-detail.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ClarityModule } from 'clarity-angular';
+import { SharedModule } from 'app/shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { MomentModule } from 'angular2-moment/moment.module';
+import { ClipboardModule } from 'ngx-clipboard/src';
+import { AppService } from 'app/services';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppDetailComponent', () => {
   let component: AppDetailComponent;
@@ -8,7 +16,19 @@ describe('AppDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppDetailComponent ]
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        HttpClientModule,
+        ClarityModule.forRoot(),
+        MomentModule,
+        ClipboardModule,
+        SharedModule
+      ],
+      declarations: [ AppDetailComponent ],
+      providers: [
+        AppService
+      ]
     })
     .compileComponents();
   }));
