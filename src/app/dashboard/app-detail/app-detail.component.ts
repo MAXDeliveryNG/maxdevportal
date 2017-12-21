ff52f4dc9c68b008aba3fcc22219014481a2994f
Patrick Foh <patrickfoh@gmail.com>
2017-12-21 16:01:30 +0100

    Refactor component

--- src/app/dashboard/app-detail/app-detail.component.ts
+++ src/app/dashboard/app-detail/app-detail.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewChild, Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppService } from 'app/services';
 import { Subscription } from 'rxjs';
+import { App } from 'app/models/data/app';
+import { AppKey } from 'app/models/data/app-key';
+import { Modal } from 'clarity-angular';
+import { AppkeyModalComponent } from 'app/dashboard/appkey-modal/appkey-modal.component';
 
 @Component({
   selector: 'app-app-detail',
@@ -9,13 +13,18 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./app-detail.component.scss']
 })
 
-export class AppDetailComponent implements OnInit {
+export class AppDetailComponent implements OnInit, OnDestroy {
 
   route$: Subscription;
   loading: boolean = true;
   loadingError: boolean = false;
-  data: any;
+  data: App;
   id: string;
+  toDelete: AppKey;
+  deleting: boolean = false;
+  @ViewChild('deleteModal') keyDeleteModal: Modal;
+  @ViewChild('keyModal') keyModal: AppkeyModalComponent;
+  errorMessage: string;
 
   constructor(
     private route: ActivatedRoute, 
@@ -29,6 +38,32 @@ export class AppDetailComponent implements OnInit {
       return key;
     })
     return response;
+  }
+
+  showDeleteModal(key: AppKey) {
+    this.keyDeleteModal.open();
+    this.toDelete = key;
+  }
+
+  deleteKey(id: string) {
+    if (this.data.keys.length < 2) {
+      this.errorMessage = "Your app must have at least one key";
+    } else {
+      this.deleting = true;
+
+      this.appService.deleteKey(this.toDelete.id)
+        .subscribe(response => {
+          this.deleting = false;
+          this.keyDeleteModal.close();
+          this.data.keys.splice(this.data.keys.indexOf(this.toDelete), 1);
+        }, err => {
+          this.deleting = false;
+        })
+    }
+  }
+
+  updateAppKeys($event: AppKey) {
+    this.data.keys.unshift($event);
   }
   
   fetchData(id: string) {
@@ -52,5 +87,16 @@ export class AppDetailComponent implements OnInit {
       this.id = params.id;
       this.fetchData(params.id);
     })
+
+    this.keyDeleteModal._openChanged.subscribe(open => {
+      if (!open) {
+        this.toDelete = null;
+        this.errorMessage = null;
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.keyDeleteModal._openChanged.unsubscribe();
   }
 }
