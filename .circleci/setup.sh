daead968cc4c82828d76dfddb0a5d671cf9c8d9d
Patrick Foh <patrickfoh@gmail.com>
2018-12-13 12:14:31 +0100

    Intial commit for branch

--- .circleci/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git remote add production git@developers.max.ng:~/
-git remote add staging git@devs-staging.max.ng:~/
-
-# Add analytics.max.ng to the list of known hosts
-ssh-keyscan -H developers.max.ng >> ~/.ssh/known_hosts
-ssh-keyscan -H devs-staging.max.ng >> ~/.ssh/known_hosts
